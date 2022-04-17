2db4d33215f840f766547e32795c078e6f36eed1
gnituy18 <sfhp@protonmail.com>
2022-04-18 00:09:06 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Rubik", "sans-serif"],
-      },
-      gridTemplateRows: {
-        11: "repeat(11, minmax(0, 1fr))",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./index.html"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Rubik", "sans-serif"],
+			},
+			gridTemplateRows: {
+				11: "repeat(11, minmax(0, 1fr))",
+			},
+			screens: {
+				"2xl": { max: "1535px" },
+				xl: { max: "1296px" },
+				lg: { max: "992px" },
+				md: { max: "768px" },
+				sm: { max: "360px" },
+			},
+		},
+	},
+	plugins: [],
 };
